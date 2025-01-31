a2d5af8a1abf50434667d885f5e6dcbc4160c03d
Bhushan Patil <bhushanlaxmanpatil3236@gmail.com>
2025-01-31 12:19:00 +0530

    todo_app

---
+++ server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    secrete: '62f48cce712cec8b6327786b365019258b1145bf4a7799e3671e2b8985f315af',
+    email: {
+      user: 'bhushanlaxmanpatil3236@gmail.com',
+      password: 'etoe wbnx xojc lnvw',
+    },
+  }
+  
